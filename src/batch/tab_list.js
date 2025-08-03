99b3b1778d9895cd4a75d3f9230a5612d1618885
Hsiehting Lin <hsieh.ting.lin@gmail.com>
2025-08-04 04:10:50 +0800

    Move appendPageNumberToSlide to src/util/page_number.js

--- src/batch/tab_list.js
+++ src/batch/tab_list.js
@@ -147,70 +147,7 @@ function appendTabListToSlide({
 	addBottomLine(slideId, requests, config);
 }
 
-/** 批次建立頁碼文字 */
-function appendPageNumberToSlide({
-	slideId,
-	requests,
-	currentPage,
-	totalPages,
-	config,
-}) {
-	const pageNumId = `page_num_${slideId}_${newGuid()}`;
-	requests.push(
-		// 建立分頁文字框
-		{
-			createShape: {
-				objectId: pageNumId,
-				shapeType: "TEXT_BOX",
-				elementProperties: {
-					pageObjectId: slideId,
-					size: {
-						height: { magnitude: 30, unit: "PT" },
-						width: { magnitude: 70, unit: "PT" },
-					},
-					transform: {
-						translateX: 650,
-						translateY: 370,
-						scaleX: 1,
-						scaleY: 1,
-						unit: "PT",
-					},
-				},
-			},
-		},
-		// 插入分頁文字
-		{
-			insertText: {
-				objectId: pageNumId,
-				text: `${currentPage} / ${totalPages}`,
-			},
-		},
-		// 文字樣式 & 對齊
-		{
-			updateTextStyle: {
-				objectId: pageNumId,
-				textRange: { type: "ALL" },
-				style: {
-					bold: true,
-					fontFamily: config.mainFont,
-					fontSize: { magnitude: 12, unit: "PT" },
-					foregroundColor: {
-						opaqueColor: { rgbColor: hexToRgb(config.inactiveTextColor) },
-					},
-				},
-				fields: "bold,fontFamily,fontSize,foregroundColor",
-			},
-		},
-		{
-			updateParagraphStyle: {
-				objectId: pageNumId,
-				textRange: { type: "ALL" },
-				style: { alignment: "CENTER" },
-				fields: "alignment",
-			},
-		},
-	);
-}
+// appendPageNumberToSlide function is now in src/util/page_number.js
 
 function appendTab({
 	slideId,
